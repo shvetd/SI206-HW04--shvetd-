024e12f85687a567492af116e2d69a690a414e82
shvetd <shvetd@umich.edu>
2018-01-31 10:09:33 -0500

    finished

--- magic_eight.py
+++ magic_eight.py
@@ -1,7 +1,7 @@
 import random
-answer = input "What is your question?"
+answer = input ("What is your question?")
 
-def answer():
+def option():
 	answers = ["It is certain", "It is decidedly so", "Without a doubt",
 	"Yes definitely", "You may rely on it", "As I see it, yes", "Most likely",
 	"Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later",
@@ -10,7 +10,8 @@ def answer():
 	num = random.randint(0,19)
 	return answers[num]
 
-while !(answer = "quit"):
-    if !(answer[-1]) = '?')
-    print "I'm sorry, I can only answer questions."
-    answer = input "What is your question?"
+while (answer != "quit"):
+    print (option())
+    if ((answer[-1]) != '?'):
+        print ("I'm sorry, I can only answer questions.")
+    answer = input ("What is your question?")
