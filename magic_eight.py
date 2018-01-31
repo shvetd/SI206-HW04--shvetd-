e3996cffa6a6a55cfd73dde1975c4fb5ae6e2afa
shvetd <shvetd@umich.edu>
2018-01-31 09:57:36 -0500

    added loop

--- magic_eight.py
+++ magic_eight.py
@@ -1,1 +1,7 @@
 answer = input "What is your question?"
+
+
+while !(answer = "quit"):
+    if !(answer[-1]) = '?')
+    print "I'm sorry, I can only answer questions."
+    answer = input "What is your question?"
